c83634c8946ede20109702d2c0f2114b3e5fc925
Lofos1610 <lofosaisystems@gmail.com>
2025-05-29 01:51:35 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,21 +3,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ padding: '2rem', background: '#f9f9f9' }}>
+      <h2>記事選択テスト</h2>
+      <p>React UI を Hot Pepper 上に表示中</p>
     </div>
   );
 }
